3d988f76493177b09c7252454d7080f078d5beb0
Angelesroque <129466272+Angelesroque@users.noreply.github.com>
2024-04-15 14:52:02 -0300

    Add files via upload

--- guia4.hs
+++ guia4.hs
@@ -15,5 +15,27 @@ parteEntera n
             | 0 <= n  && n < 1 = 0
             | n >= 1 = parteEntera(n - 1) +1 
             |otherwise = parteEntera(n+1)-1
-            
+
+--3)
+esDivisible :: Integer -> Integer -> Bool
+esDivisible x z 
+               | x - z == 0 = True
+               | x - z < 0  = False
+               |otherwise = esDivisible(x - z) z 
+
+--4)
+sumaImpares :: Integer -> Integer
+sumaImpares n 
+            | n == 0 = 0
+            | n == 1 = 1
+            |otherwise =  sumaImpares(n - 1) + (n*2-1)
+
+sumaImpAux :: Integer -> Integer -> Integer 
+sumaImpAux n m 
+              | n == 0 = 0
+              | n == 1 = 1
+              | mod m 2 == 0 = sumaImpAux n (m+1)
+              | otherwise = 
+                
+
 
